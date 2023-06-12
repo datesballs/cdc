97ba44ffb2cd948e36702a92549a38b162673aae
LauraKathrine <10407725@ucn.dk>
2023-06-12 11:48:42 +0200

    Animation
    Ændring af forretning og tilføjelse af animation

--- Carlsens dykkercenter/assets/script/script.js
+++ Carlsens dykkercenter/assets/script/script.js
@@ -41,3 +41,19 @@ const swiper = new Swiper('.swiper', {
     }
 });
 
+
+// Båd animationen
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else{
+            entry.target.classList.remove('show')
+        }
+    }); {rootMargin: "0px 0px -500px 0px"}
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
